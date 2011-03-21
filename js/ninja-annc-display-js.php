f029f2bf1e0f486d17b0f41b3d1a75078524dec2
kstover <kstover@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-21 18:34:40 +0000

    Ninja Announcements Version 1.2. Fixed some minor bugs.
    git-svn-id: https://plugins.svn.wordpress.org/ninja-announcements/tags/1.2@362978 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ninja-annc-display-js.php
@@ -0,0 +1,23 @@
+<?php
+$plugin_url = $_REQUEST['plugin_url'];
+$plugin_url = str_replace("(", "", $plugin_url);
+$plugin_url = str_replace(")", "", $plugin_url);
+$plugin_url = str_replace(";", "", $plugin_url);
+
+?>
+
+jQuery(document).ready(function($) {
+	$(".ninja_annc_close").click(function(event){
+		var plugin_url = <?php echo $plugin_url;?>
+		event.preventDefault();
+		var cont_id = this.id.replace("close_", "");
+		var annc_id = this.id.replace("close_ninja_annc_container_", "");
+		$.post( plugin_url + "/include/process.php", { action: "close", annc_id: annc_id },
+			function(data){
+				
+			}
+		);
+		$("#" + cont_id).hide("explode");
+	});
+
+});
