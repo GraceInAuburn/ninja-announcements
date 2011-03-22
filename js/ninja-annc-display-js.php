68eddc28e8a4e08a7a90aece74614a6b697d0df3
kstover <kstover@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-22 14:19:23 +0000

    Bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/ninja-announcements/tags/1.2@363267 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ninja-annc-display-js.php
+++ js/ninja-annc-display-js.php
@@ -1,14 +1,12 @@
 <?php
 $plugin_url = $_REQUEST['plugin_url'];
-$plugin_url = str_replace("(", "", $plugin_url);
-$plugin_url = str_replace(")", "", $plugin_url);
-$plugin_url = str_replace(";", "", $plugin_url);
+$plugin_url = htmlentities($plugin_url);
 
 ?>
 
 jQuery(document).ready(function($) {
 	$(".ninja_annc_close").click(function(event){
-		var plugin_url = <?php echo $plugin_url;?>
+		var plugin_url = "<?php echo $plugin_url;?>";
 		event.preventDefault();
 		var cont_id = this.id.replace("close_", "");
 		var annc_id = this.id.replace("close_ninja_annc_container_", "");
