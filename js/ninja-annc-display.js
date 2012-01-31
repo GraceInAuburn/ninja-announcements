0c683a85884c2cefb4224150e46c3a92735e21ca
kstover <kstover@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-31 15:45:40 +0000

    Preparing for new release.
    git-svn-id: https://plugins.svn.wordpress.org/ninja-announcements/trunk@497976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ninja-annc-display.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	$(".ninja_annc_close").click(function(event){
-		event.preventDefault();
-		var cont_id = this.id.replace("close_", "");
-		var annc_id = this.id.replace("close_ninja_annc_container_", "");
-		$("#" + cont_id).hide("explode");
-	});
-
-});
